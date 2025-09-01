b71bfd839a99d68a330883c789be018f2e5a36d2
J. Keith Lawson <keith@nowhere.ca>
2025-09-01 13:37:05 -0400

    Unit 1 assignment

---
+++ unit1.py
@@ -0,0 +1,17 @@
+# Virtual display
+from pyvirtualdisplay import Display
+import gymnasium
+from huggingface_sb3 import load_from_hub, package_to_hub
+from huggingface_hub import (
+    notebook_login,
+)  # To log to our Hugging Face account to be able to upload models to the Hub.
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+
+# Setup virtual display
+virtual_display = Display(visible=0, size=(1400, 900))
+virtual_display.start()
+
+
