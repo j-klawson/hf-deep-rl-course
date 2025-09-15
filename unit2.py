d90c542c34040ae26fb45b4af4cd5b72b86151ec
J. Keith Lawson <keith@nowhere.ca>
2025-09-15 15:25:21 -0400

    Implemented training

--- unit2.py
+++ unit2.py
@@ -5,9 +5,8 @@ import gymnasium as gym
 import random
 import imageio
 import os
-import tqdm
 import pickle
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 from pyvirtualdisplay import Display
 
 # Training parameters
@@ -37,7 +36,7 @@ def initialize_q_table(state_space, action_space):
 def greedy_policy(Qtable, state):
   # Exploitation: take the action with the highest state, action value
   action = np.argmax(Qtable[state][:])
-  return ac
+  return action
 
 def epsilon_greedy_policy(Qtable, state, epsilon):
   # Randomly generate a number between 0 and 1
@@ -52,6 +51,38 @@ def epsilon_greedy_policy(Qtable, state, epsilon):
     action = env.action_space.sample()
 
   return action
+
+def train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable):
+  for episode in tqdm(range(n_training_episodes)):
+    # Reduce epsilon (because we need less and less exploration)
+    epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
+    # Reset the environment
+    state, info = env.reset()
+    step = 0
+    terminated = False
+    truncated = False
+
+    # repeat
+    for step in range(max_steps):
+      # Choose the action At using epsilon greedy policy
+      action = epsilon_greedy_policy(Qtable, state, epsilon)
+
+      # Take action At and observe Rt+1 and St+1
+      # Take the action (a) and observe the outcome state(s') and reward (r)
+      new_state, reward, terminated, truncated, info = env.step(action)
+
+      # Update Q(s,a):= Q(s,a) + lr [R(s,a) + gamma * max Q(s',a') - Q(s,a)]
+      Qtable[state][action] = Qtable[state][action] + learning_rate * (
+        reward + gamma * np.max(Qtable[new_state]) - Qtable[state][action]
+       )
+
+      # If terminated or truncated finish the episode
+      if terminated or truncated:
+        break
+
+      # Our next state is the new state
+      state = new_state
+  return Qtable
 
 # 
 # Main program
@@ -83,3 +114,9 @@ action_space = env.action_space.n
 print("There are ", action_space, " possible actions")
 
 Qtable_frozenlake = initialize_q_table(state_space, action_space)
+print("Q-table Init:")
+print (Qtable_frozenlake, "\n")
+Qtable_frozenlake = train(n_training_episodes, min_epsilon, max_epsilon, decay_rate, env, max_steps, Qtable_frozenlake)
+# Let's see what our Q-Learning table looks like now 👀
+print("\nQ-table after training:")
+print("\n", Qtable_frozenlake, "\n")
